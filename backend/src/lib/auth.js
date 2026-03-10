34da1df175971f8a29e2f187cf2dbb4f1cc64d11
fengliuz <fengliuzng4a@gmail.com>
2026-03-10 12:22:01 +0700

    berhasil test 1 b

--- backend/src/lib/auth.js
+++ backend/src/lib/auth.js
@@ -3,26 +3,41 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth2";
 import dotenv from "dotenv";
 import { User } from "../Models/User.js";
 dotenv.config();
+
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5001/google/callback",
-  }),
-  async function (accessToken, refreshToken,profile,cb) {
-    try {
-        let user = await User.findOne({googleId:profile.id})
-        if(!user){
-            user = await User.create({
-                username:profile.given_name || profile.displayName,
-                googleId:profile.id,
-                email:profile.emails[0].value,
-                avatar:profile.photos[0].value,
-            })
-            return cb
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5001/google/callback",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            username: profile.given_name || profile.displayName,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+          });
         }
-    } catch (error) {
-        
-    }
-  },
+        return cb(null, user);
+      } catch (error) {
+        cb(error, null);
+      }
+    },
+  ),
 );
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+passport.deserializeUser(async (id, cb) => {
+  try {
+    const user = await User.findById(id);
+    cb(null, user);
+  } catch (error) {
+    cb(error, null);
+  }
+});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,41 @@
-import express from "express"
-import dotenv from "dotenv"
-import UserRouter from "./Routers/UserRouter.js"
-import connectDB from "./lib/db.js"
-dotenv.config()
-const app = express()
-app.use(express.json())
-connectDB()
+import express from "express";
+import dotenv from "dotenv";
+import UserRouter from "./Routers/UserRouter.js";
+import connectDB from "./lib/db.js";
+import passport from "passport";
+import session  from "express-session";
+import "./lib/auth.js"
+dotenv.config();
+const app = express();
+app.use(express.json());
+connectDB();
+app.use(session({
+    resave:false,saveUninitialized:false,secret:"amba",cookie:{secure:false}
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use("/api", UserRouter);
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'>Authorize</a>");
+});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile","email"] }),
+);
+app.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth/error" }),async(req,res)=>{
+    res.redirect("/dashboard")
+  }
+);
 
-app.use("/api",UserRouter)
-
-app.listen(process.env.PORT,()=>{
-    console.log("object")
+app.get("/auth/error",(req,res)=>{
+    res.send("<p>err</p>")
 })
+app.get("/dashboard",(req,res)=>{
+    res.send("<p>aaaaa</p>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log("object");
+});
